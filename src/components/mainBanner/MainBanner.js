cbdee59ad3a0b9737cb4e751fa679b0a9a2f66b8
sauravraj276 <sauravraj276@gmail.com>
2024-01-17 18:43:03 +0530

    text added over banner

--- src/components/mainBanner/MainBanner.js
+++ src/components/mainBanner/MainBanner.js
@@ -1,9 +1,14 @@
-import React from 'react'
-import style from './MainBanner.module.css'
-import banner from '../../'
+import React from "react";
+import style from "./MainBanner.module.css";
 
 export default function MainBanner() {
   return (
-    <div className={style.mainbanner} >MainBanner</div>
-  )
+    <div className={style.mainbanner}>
+      <div className={style.mainText}>
+        <p className={style.mainTitle}>EXPLOREX</p>
+        <p className={style.mainDesc}>Discover SpaceX DATA effortlessly with our API-powered site.</p>
+      </div>
+      <div className="scrollDown"></div>
+    </div>
+  );
 }
