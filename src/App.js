bb6087acf3300cfc51a318aa5ca685e074d292c1
sauravraj276 <sauravraj276@gmail.com>
2024-01-17 21:38:22 +0530

    background image added in search section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/navbar/Navbar';
-import MainBanner from './components/mainBanner/MainBanner';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import MainBanner from "./components/mainBanner/MainBanner";
+import SearchSection from "./components/searchSection/SearchSection1";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <MainBanner/>
+      <Navbar />
+      <MainBanner />
+      <SearchSection />
     </div>
   );
 }
---
+++ src/components/searchSection/SearchSection1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './SearchSection1.module.css'
+
+export default function SearchSection1() {
+  return (
+    <div className={style.searchSection1}>
+      
+    </div>
+  )
+}
