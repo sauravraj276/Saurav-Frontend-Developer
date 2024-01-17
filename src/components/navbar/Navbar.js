91b9ad2754d636e7508846c7ac7760f650a20bee
sauravraj276 <sauravraj276@gmail.com>
2024-01-17 19:23:53 +0530

    navbar completed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import style from './Navbar.module.css'
+import searchIcon from '../../assets/icons/search_light.svg'
 
 export default function Navbar() {
   return (
-    <nav className={style.nav}></nav>
+    <nav className={style.nav}>
+      <p className={style.navTitle}>
+        EXPLOREX
+      </p>
+      <div className={style.navAction}>
+        <img src={searchIcon}></img>
+      </div>
+    </nav>
   )
 }
